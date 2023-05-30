c051fc1d3686a253678cd655190404d658a4e7ee
Torres Martín <martin.torres98@hotmail.com>
2023-05-30 18:44:06 -0300

    Agregué el test que comprueba si se guarda en el historial una división hecha por el endpoint

--- test/models.test.js
+++ test/models.test.js
@@ -27,4 +27,24 @@ describe("History", () => {
         expect(histories[0].result).toEqual(0)
         expect(histories[0].Operation.name).toEqual("SUB")
     })
+
+    test("Debería poder crear una división en el history", async() => {
+      await createHistoryEntry({
+        firstArg: 27,
+        secondArg: 3,
+        result: 9,
+        operationName: "DIV"
+      })
+
+      const entries = await History.findAll({
+        include: [Operation]
+      })
+
+      expect(entries.length).toEqual(9)
+      expect(entries[0].firstArg).toEqual(27)
+      expect(entries[0].secondArg).toEqual(3)
+      expect(entries[0].result).toEqual(9)
+      expect(entries[0].Operation.name).toEqual()
+
+    })
 })
