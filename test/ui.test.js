01ec4c0b225534e222a3eaeab9f33159edcdd584
mmilagros.96m <mmilagros.96m@gmail.com>
2023-06-15 19:07:57 -0300

    realice el test para la suma en la interfaz

--- test/ui.test.js
+++ test/ui.test.js
@@ -48,4 +48,37 @@ test.describe('test', () => {
     expect(historyEntry.secondArg).toEqual(9)
     expect(historyEntry.result).toEqual(70)
   });
+
+  test('Deberia poder realizar una suma', async ({ page }) => {
+    await page.goto('./');
+
+    await page.getByRole('button', { name: '2' }).click()
+    await page.getByRole('button', { name: '4' }).click()
+    await page.getByRole('button', { name: '+' }).click()
+    await page.getByRole('button', { name: '1' }).click()
+
+    const [response] = await Promise.all([
+      page.waitForResponse((r) => r.url().includes('/api/v1/add/')),
+      page.getByRole('button', { name: '=' }).click()
+    ]);
+
+    const { result } = await response.json();
+    expect(result).toBe(25);
+
+    await expect(page.getByTestId('display')).toHaveValue(/25/)
+
+    const operation = await Operation.findOne({
+      where: {
+        name: "ADD"
+      }
+    });
+
+    const historyEntry = await History.findOne({
+      where: { OperationId: operation.id }
+    })
+
+    expect(historyEntry.firstArg).toEqual(24)
+    expect(historyEntry.secondArg).toEqual(1)
+    expect(historyEntry.result).toEqual(25)
+  });
 })
