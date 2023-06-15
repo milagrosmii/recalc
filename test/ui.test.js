72fc5efcfdce5104576007c540c5d1f29c94e506
fidelaguirre <fidelaguirre1998@gmail.com>
2023-06-15 16:12:34 -0300

    Agregué los test correspondientes de la ui para la funcion pow -Se testea que el resultado de una operacion sea correcto -Se testea que se muestre un error en el display si el resultado supera 100.000

--- test/ui.test.js
+++ test/ui.test.js
@@ -48,4 +48,71 @@ test.describe('test', () => {
     expect(historyEntry.secondArg).toEqual(9)
     expect(historyEntry.result).toEqual(70)
   });
+
+  //TEST: Verifica que la potencia cuadrada se calcule correctamente
+  test('Deberia poder calcular la potencia cuadrada de un número', async ({page}) => {
+    await page.goto('./');
+
+
+    await page.getByRole('button', { name: '7' }).click();
+    await page.getByRole('button', { name: '^2' }).click();
+
+    const [response] = await Promise.all([
+      page.waitForResponse((r) => r.url().includes('/api/v1/pow/')),
+      page.getByRole('button', { name: '=' }).click()
+    ]);
+
+    const { result } = await response.json();
+    expect(result).toBe(49);
+
+    await expect(page.getByTestId('display')).toHaveValue(/49/)
+
+    const operation = await Operation.findOne({
+      where: {
+        name: "POW"
+      }
+    });
+
+    const historyEntry = await History.findOne({
+      where: { OperationId: operation.id }
+    })
+
+    expect(historyEntry.firstArg).toEqual(7)
+    expect(historyEntry.result).toEqual(49)
+  });
+
+  //TEST: Verifica que la calculadora tire error si el resultado es mayor que 100.000
+  test('Verifica que la calculadora tire error si el resultado es mayor que 100.000', async ({page}) => {
+    await page.goto('./');
+
+
+    await page.getByRole('button', { name: '1' }).click();
+    await page.getByRole('button', { name: '0' }).click();
+    await page.getByRole('button', { name: '0' }).click();
+    await page.getByRole('button', { name: '0' }).click();
+    await page.getByRole('button', { name: '^2' }).click();
+
+    const [response] = await Promise.all([
+      page.waitForResponse((r) => r.url().includes('/api/v1/pow/')),
+      page.getByRole('button', { name: '=' }).click()
+    ]);
+
+    const { result } = await response.json();
+    expect(result).toBe(1000000);
+
+    await expect(page.getByTestId('display')).toHaveValue(/Error: Valor muy grande/)
+
+    const operation = await Operation.findOne({
+      where: {
+        name: "POW"
+      }
+    });
+
+    const historyEntry = await History.findOne({
+      where: { OperationId: operation.id }
+    })
+
+    expect(historyEntry.firstArg).toEqual(1000)
+    expect(historyEntry.result).toEqual(1000000)
+  });
 })
