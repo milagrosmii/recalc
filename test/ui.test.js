f4eeed2b30e1673ca55dfcde4e9438968456cf3b
Torres Martín <martin.torres98@hotmail.com>
2023-06-17 02:16:21 -0300

    Agregué el test de división para la UI

--- test/ui.test.js
+++ test/ui.test.js
@@ -49,6 +49,42 @@ test.describe('test', () => {
     expect(historyEntry.result).toEqual(70)
   });
 
+  //Debería realizar una división
+  test('Debería poder realizar una división', async ({ page }) => {
+    await page.goto('./');
+
+    await page.getByRole('button', {name: '9' }).click()
+    await page.getByRole('button', {name: '/' }).click()
+    await page.getByRole('button', {name: '3' }).click()
+
+    const [response] = await Promise.all([
+      page.waitForResponse((r) => r.url().includes('/api/v1/div/')),
+      page.getByRole('button', { name: '=' }).click()
+    ]);
+
+    const { result } = await response.json();
+    expect(result).toBe(3);
+
+    await expect(page.getByTestId('display')).toHaveValue(/3/)
+
+    const operation = await Operation.findOne({
+      where: {
+        name: "DIV"
+      }
+    });
+
+    const historyEntry = await History.findOne({
+      where: { OperationId: operation.id }
+    })
+
+    expect(historyEntry.firstArg).toEqual(9)
+    expect(historyEntry.secondArg).toEqual(3)
+    expect(historyEntry.result).toEqual(3)
+
+  })
+
+  
+
   //Deberia borrar el contenido del display al pulsar el boton C
   test('Deberia limpiar el display al pulsar el boton C', async ({ page }) => {
     await page.goto('./');
